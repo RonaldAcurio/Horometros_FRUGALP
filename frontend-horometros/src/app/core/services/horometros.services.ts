d0e2203bed968a666e9fb550b3fcc6ba83262178
RonaldAcurio <diazsteeven123@gmail.com>
2026-09-01 19:24:28 -0500

    fix: actualizaciones

--- frontend-horometros/src/app/core/services/horometros.services.ts
+++ frontend-horometros/src/app/core/services/horometros.services.ts
@@ -2,13 +2,15 @@ import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { RespuestaLote, ConfirmarIngresoPayload, RespuestaConfirmacion } from '../models/horometro.model';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HorometrosService {
   private http = inject(HttpClient);
-  private apiUrl = 'http://localhost:3000/api/horometros'; // Ajusta según tu puerto backend
+  //private apiUrl = 'http://localhost:3000/api/horometros'; // Ajusta según tu puerto backend
+  private apiUrl = environment.apuUrl;
 
   // Subir hasta 6 imágenes en lote
   procesarLote(archivos: File[]): Observable<RespuestaLote> {
---
+++ frontend-horometros/src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: true,
+    apuUrl: 'https://horometros-frugalp.onrender.com/api/horometros'
+    //local
+    //apiUrl: 'http://localhost:3000/api/horometros'
+};
