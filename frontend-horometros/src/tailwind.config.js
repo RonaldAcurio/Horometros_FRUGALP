47f69c2f14fed311a7f36ccd890ba794b36bec03
RonaldAcurio <diazsteeven123@gmail.com>
2026-09-09 14:11:11 -0500

    fix:cambio de diseno por uno mas semejante a la empresa

--- frontend-horometros/src/tailwind.config.js
+++ frontend-horometros/src/tailwind.config.js
@@ -4,7 +4,23 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // "Concho de Vino" - acento de marca FRUGALP para estados de urgencia / destructivos
+        vino: {
+          50: '#fbeef1',
+          100: '#f5d7de',
+          200: '#e8adbd',
+          300: '#d47f97',
+          400: '#b8506f',
+          500: '#973256',
+          600: '#7a1f3d',
+          700: '#5c1730',
+          800: '#421022',
+          900: '#2b0a16',
+        },
+      },
+    },
   },
   plugins: [],
 }
